e7e88fa3c81ab90e794b1cb96ee685ce08e98cdb
araujodanield <daniel-araujo2021@outlook.com>
2023-01-24 19:56:33 -0300

    Test the interaction with the display and the buttons 'clear' and 'one'

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("test");
+const display = document.querySelector(".display");
+
+const btnOne = document.getElementById("one");
+
+const btnClear = document.getElementById("clear");
+
+let changeDisplay = () => {
+    btnOne.addEventListener("click", () => {
+        display.textContent = "1";
+    });
+}
+
+let clearDisplay = () => {
+    btnClear.addEventListener("click", () => {
+        display.textContent = "";
+    })
+}
+
+changeDisplay()
+clearDisplay()
