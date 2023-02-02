ea2641e65722c6553d1e7cedcd55487efd2c551b
araujodanield <daniel-araujo2021@outlook.com>
2023-02-02 00:39:36 -0300

    Fix the comma button to allow the sign only after the input has already a number

--- script.js
+++ script.js
@@ -2,26 +2,36 @@ const operation = document.querySelector(".operation");
 const input = document.querySelector(".user-input");
 const buttons = document.querySelectorAll("button");
 
-
+let value;
 changeDisplay = () => {
     buttons.forEach((btn) => {
         btn.addEventListener("click", (e) => {
-            let value = e.target.textContent;
+            value = e.target.textContent;
 
-            if (+value >= 0 || value === ",") {
-                if (value === "," && input.textContent.includes(",")) {return};
+            if (+value >= 0) {
                 input.textContent += value;
-            } else if (value === "+ / -") {
-                if (value === "+ / -" && input.textContent.includes("-")) {return};
+
+            } else if (value === ",") {
+                if (input.textContent.includes(",") || !+input.textContent) {
+                    return;
+                } else {
+                    input.textContent += value;
+                }
+
+            } else if (value === "+/-") {
+                if (value === "+/-" && input.textContent.includes("-")) {return};
                 input.textContent = "-" + input.textContent;
+
             } else if (value === "⌫") {
                 input.textContent = input.textContent.slice(0, -1);
+
             } else if (value === "C") {
-                operation.textContent = ""
-                input.textContent = ""
+                operation.textContent = "";
+                input.textContent = "";
+
             } else {
-                operation.textContent += input.textContent + " " + value + " "
-                input.textContent = ""
+                operation.textContent += input.textContent + " " + value + " ";
+                input.textContent = "";
             }
         })
     })
