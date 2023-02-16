1246e75c8604c2be6a99968142ed8b0f4de6637c
araujodanield <daniel-araujo2021@outlook.com>
2023-02-15 21:47:04 -0300

    Refactor the code with a different approach to make the operations

--- script.js
+++ script.js
@@ -1,57 +1,177 @@
-const operation = document.querySelector(".operation");
+// const operation = document.querySelector(".operation");
+// const input = document.querySelector(".user-input");
+// const buttons = document.querySelectorAll("button");
+
+// let value;
+
+// changeDisplay = () => {
+//     buttons.forEach((btn) => {
+//         btn.addEventListener("click", (e) => {
+//             value = e.target.textContent;
+
+//             if (+value >= 0) {
+//                 input.textContent += value;
+
+//             } else if (value === ",") {
+//                 if (input.textContent.includes(",")) {
+//                     return;
+//                 } else {
+//                     input.textContent += value;
+//                 }
+
+//             } else if (value === "+/-") {
+//                 if (value === "+/-" && input.textContent.includes("-")) {return};
+//                 input.textContent = "-" + input.textContent;
+
+//             } else if (value === "⌫") {
+//                 input.textContent = input.textContent.slice(0, -1);
+
+//             } else if (value === "C") {
+//                 operation.textContent = "";
+//                 input.textContent = "";
+
+//             } else {
+//                 if (input.textContent === "") {return};
+//                 operation.textContent += `${input.textContent} ${value} `;
+//                 input.textContent = "";
+//             }
+//         })
+//     })
+// };
+
+// checkRules = () => {
+//     buttons.forEach((btn) => {
+//         btn.addEventListener("click", (e) => {
+
+//             // Check if the input has more than 16 digits
+//             if (input.textContent.length >= 16) {
+//                 input.textContent = input.textContent.substring(0, 16);
+//             }
+//         })
+//     })
+// };
+
+// operate = () => {};
+
+// changeDisplay();
+// checkRules();
+
+const currentOperation = document.querySelector(".operation");
 const input = document.querySelector(".user-input");
-const buttons = document.querySelectorAll("button");
 
-let value;
-
-changeDisplay = () => {
-    buttons.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
-            value = e.target.textContent;
-
-            if (+value >= 0) {
-                input.textContent += value;
-
-            } else if (value === ",") {
-                if (input.textContent.includes(",")) {
-                    return;
-                } else {
-                    input.textContent += value;
-                }
-
-            } else if (value === "+/-") {
-                if (value === "+/-" && input.textContent.includes("-")) {return};
-                input.textContent = "-" + input.textContent;
-
-            } else if (value === "⌫") {
-                input.textContent = input.textContent.slice(0, -1);
-
-            } else if (value === "C") {
-                operation.textContent = "";
-                input.textContent = "";
-
-            } else {
-                if (input.textContent === "") {return};
-                operation.textContent += `${input.textContent} ${value} `;
-                input.textContent = "";
-            }
-        })
-    })
+const buttons = {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "comma": ",",
+    "plus-minus": "+/-",
+    "add": "+",
+    "subtract": "-",
+    "multiply": "x",
+    "divide": "÷",
+    "delete": "⌫",
+    "clear": "C",
+    "equal": "=",
 };
 
-checkRules = () => {
-    buttons.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
+let currentNumber = "";
+let currentResult = 0;
+let operator = "";
+let isNegative = false;
 
-            // Check if the input has more than 16 digits
-            if (input.textContent.length >= 16) {
-                input.textContent = input.textContent.substring(0, 16);
-            }
-        })
-    })
+
+
+for (let id in buttons) {
+    const btn = document.getElementById(id);
+    const value = buttons[id];
+    btn.addEventListener("click", () => changeDisplay(value));
 };
 
-operate = () => {};
+function changeDisplay(value) {
+    if (typeof value === "number" || value === ",") {
+        addNumber(value);
+    } else if (value === "+" || value === "-" || value === "x" || value === "÷") {
+        addOperator(value);
+    } else if (value === "+/-") {
+        switchNegative();
+    } else if (value === "⌫") {
+        deleteNumber();
+    } else if (value === "C") {
+        clearAll();
+    } else if (value === "=") {
+        operate();
+    };
+
+    //Check if the input has more than 16 digits
+    if (input.textContent.length >= 16) {
+        input.textContent = input.textContent.substring(0, 16);
+    };
+};
+
+function addNumber(number) {
+    if (number === "," && currentNumber.includes(",")) {return};
+    if (number === "," && currentNumber === "") {currentNumber = 0};
+    currentNumber += number;
+    input.textContent = currentNumber;
+};
+
+function addOperator(newOperator) {
+    operator = newOperator;
+    currentResult = Number(currentNumber.replace(",","."));
+    currentNumber = "";
+};
+
+function switchNegative() {
+    isNegative = !isNegative;
+    if (isNegative) {
+        currentNumber = "-" + currentNumber;
+    } else {
+        currentNumber = currentNumber.slice(1);
+    };
+    input.textContent = currentNumber;
+};
+
+function deleteNumber() {
+    currentNumber = currentNumber.slice(0, -1);
+    input.textContent = currentNumber;
+};
+
+function clearAll() {
+    currentNumber = "";
+    operator = "";
+    currentResult = 0;
+    input.textContent = "";
+    currentOperation.textContent = "";
+};
+
+function operate() {
+    const number = Number(currentNumber.replace(",","."));
+    switch (operator) {
+        case "+":
+            currentResult += number;
+            break;
+        case "-":
+            currentResult -= number;
+            break;
+        case "x":
+            currentResult *= number;
+            break;
+        case "÷":
+            currentResult /= number;
+            break;
+        default:
+            currentResult = "";
+    }
+    currentNumber = currentResult.toString().replace(".",",");
+    operator = "";
+    input.textContent = currentResult.toString().replace(".",",");
+};
 
 changeDisplay();
-checkRules();
